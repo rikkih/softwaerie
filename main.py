c450c29714d9b299ff45c02ce8cbea6caf18b66d
Rikki Hodder <rikkih@protonmail.com>
2021-05-12 21:53:36 +0100

    just playing around with FastAPI atm. Nothing special.

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"}
+]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 0):
+    return fake_items_db[skip: skip + limit]
 
