716e1e06d3131364beac61ed09ef9fe75502d6c9
Rikki Hodder <rikkih@protonmail.com>
2021-06-13 13:31:21 +0100

    action script for requests

---
+++ app/actions.py
@@ -0,0 +1,79 @@
+from typing import Any, Dict, Generic, List, Optional, Type, TypeVar, Union
+
+from fastapi.encoders import jsonable_encoder
+from pydantic import UUID4, BaseModel
+from sqlalchemy.orm import Session
+
+from . import schemas
+from .db import Base
+from .models import Post
+
+# bound argument means any type must be a subclass of BaseModel
+ModelType = TypeVar("ModelType", bound=Base)
+CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
+UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
+
+
+class BaseActions(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
+    def __init__(self, model: Type[ModelType]):
+        """For Basic CRUD Operations. Extendable.
+
+            Params:
+                model: SQLAlchemy Model
+        """
+        self.model = model
+
+    def get_all(
+        self,
+        db: Session,
+        *,
+        skip: int = 0,
+        limit: int = 100
+    ) -> List[ModelType]:
+        return db.query(self.model).offset(skip).limit(limit).all()
+
+    def get(self, db: Session, id: UUID4) -> Optional[ModelType]:
+        return db.query(self.model).filter(self.model.id == id).first()
+
+    def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
+        obj_in_data = jsonable_encoder(obj_in)
+        db_obj = self.model(**obj_in_data)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def update(
+        self,
+        db: Session,
+        *,
+        db_obj: ModelType,
+        obj_in: Union[UpdateSchemaType, Dict[str, Any]]
+    ) -> ModelType:
+        obj_data = jsonable_encoder(db_obj)
+        if isinstance(obj_in, dict):
+            update_data = obj_in
+        else:
+            update_data = obj_in.dict(exclude_unset=True)
+        for field in obj_data:
+            if field in update_data:
+                setattr(db_obj, field, update_data[field])
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def remove(self, db: Session, *, id: UUID4) -> ModelType:
+        obj = db.qeury(self.model).get(id)
+        db.delete(obj)
+        db.commit()
+        return obj
+
+
+class PostActions(BaseActions[Post, schemas.PostCreate, schemas.PostUpdate]):
+    """Post Actions with basic CRUD."""
+    pass
+
+
+post = PostActions(Post)
+
